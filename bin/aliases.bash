0681f35a39eb8a247091680833e080d162fc6309
Samer Masry <samer.masry@gmail.com>
2017-10-13 10:18:48 -0700

    Adding rdba for all db commands

--- bin/aliases.bash
+++ bin/aliases.bash
@@ -6,6 +6,8 @@ alias c='r console'
 alias s='r server -b 0.0.0.0'
 alias rdb='r db:environment:set db:drop db:create db:migrate RAILS_ENV=development'
 alias rdbt='r db:environment:set db:drop db:create db:migrate RAILS_ENV=test'
+alias rdbs='r db:environment:set db:seed RAILS_ENV=development'
+alias rdba='rdb && rdbs; rdbt'
 alias f='foreman start'
 
 alias tl="bundle exec testrbl -I test"
